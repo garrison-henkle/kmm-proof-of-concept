2aebafde75e02035c2a1bbd0de88fdc05b038dda
Garrison Henkle <ghenkle50@gmail.com>
2022-12-05 10:05:22 -0600

    KMM SPM package release for unspecified.1

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let remoteKotlinUrl = "https://api.github.com/repos/garrison-henkle/kmm-proof-of-concept/releases/assets/87084050.zip"
+let remoteKotlinChecksum = "f796e63d9df41d10035a3dad924060b124e39913250a665d1f72605167b460cd"
+let packageName = "shared"
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
