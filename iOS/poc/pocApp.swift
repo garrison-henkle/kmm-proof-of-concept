d003f0fd2c0c74972229d59a7267dd7f8550237e
Garrison Henkle <ghenkle50@gmail.com>
2022-12-05 09:20:32 -0600

    remove subgit project

---
+++ iOS/poc/pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocApp.swift
+//  poc
+//
+//  Created by Garrison Henkle on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
