ecfe6bd3349169ffeb0d0decad18c4260c5462e0
Garrison Henkle <ghenkle50@gmail.com>
2022-12-13 20:38:26 -0600

    KMM SPM package release for 0.0.17

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://api.github.com/repos/garrison-henkle/kmm-proof-of-concept/releases/assets/87503786.zip"
-let remoteKotlinChecksum = "1ed6beddd19efcc8e334d0801b2fc072cfe3a539f67a23146a2d84400c75c485"
+let remoteKotlinUrl = "https://api.github.com/repos/garrison-henkle/kmm-proof-of-concept/releases/assets/88122498.zip"
+let remoteKotlinChecksum = "0794a98e5c99460ed45fa6b40bd799eef530eac7d8eece130757c91aac893256"
 let packageName = "YouComponents"
 
 let package = Package(
---
+++ shared/src/commonMain/kotlin/com/you/components/utils/Closable.kt
@@ -0,0 +1,4 @@
+package com.you.components.utils
+
+interface Closable {
+}
